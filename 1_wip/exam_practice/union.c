1d57c783d5d528cede284664d984f6226d939ca2
abraekev <abraekev@student.s19.be>
2024-02-28 22:20:10 +0100

    azefaf

--- 1_wip/exam_practice/union.c
+++ 1_wip/exam_practice/union.c
@@ -44,14 +44,26 @@ $>
 
 */
 
-int	comes_before(char *str, int i)
+#include <unistd.h>
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = -1;
+	while (s[++i])
+	;
+	return (i);
+}
+
+int	comes_before(char *str, int i, char c)
 {
 	int	j;
 
 	j = -1;
 	while (++j < i)
 	{
-		if (str[i] == str[j])
+		if (c == str[j])
 			return (1);
 	}
 	return (0);
@@ -60,27 +72,27 @@ int	comes_before(char *str, int i)
 void	ft_union(char *one, char *two)
 {
 	int i;
-	int	j;
 
 	i = -1;
 	while (one[++i])
 	{
-		if (!comes_before(one, i))
+		if (!comes_before(one, i, one[i]))
 			write(1, &one[i], 1);
 	}
 	i = -1;
 	while (two[++i])
 	{
-		if (!comes_before(one,ft_strlen(one)) && ! comes_before(two, i))
+		if (!comes_before(one,ft_strlen(one), two[i]) && ! comes_before(two, i, two[i]))
 			write(1, &two[i], 1);
 	}
+	write(1,"\n",1);
 }
 
 int	main(int c, char **v)
 {
 	if (c != 3)
 	{
-		ft_putstr("\n");
+		write(1,"\n",1);
 		return 0;
 	}
 	ft_union(v[1], v[2]);
