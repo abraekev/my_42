7c93b7a61b3e0f3ac00a0857388fb974e6717efd
Arnout Braekevelt <abraekev@a1-r2-p8.s19.be>
2024-04-10 14:13:46 +0200

    organizing

---
+++ 2_Personal/Libft+/ft_strrchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abraekev <abraekev@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 11:12:52 by abraekev          #+#    #+#             */
+/*   Updated: 2024/04/10 13:22:15 by abraekev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	const char	*tmp;
+
+	if (!s)
+		return (NULL);
+	tmp = NULL;
+	while (*s)
+	{
+		if (*s == (char)c)
+			tmp = s;
+		s++;
+	}
+	if (!c)
+		return ((char *)s);
+	return ((char *)tmp);
+}
