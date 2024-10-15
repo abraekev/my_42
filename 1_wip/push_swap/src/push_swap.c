f3a8fa1c11854e57f7be7ef36c907a43e8876b6e
abraekev <arnout.braekevelt@gmail.com>
2024-10-15 15:13:32 +0200

    kinda working

--- 1_wip/push_swap/src/push_swap.c
+++ 1_wip/push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: abraekev <abraekev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 13:52:07 by abraekev          #+#    #+#             */
-/*   Updated: 2024/10/15 11:42:32 by abraekev         ###   ########.fr       */
+/*   Updated: 2024/10/15 15:13:20 by abraekev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int	main(int argc, char **argv)
 	init_data(validate_arguments(argc, argv), argc, argv, &data);
 	sort(&data);
 	print_log(&data);
+	// print_stack(&data.a);
 	free_data(&data);
 	exit(EXIT_SUCCESS);
 }
