24aa6a58efb6a7dc54e469368a68bbaa13516c09
abraekev <arnout.braekevelt@gmail.com>
2024-10-15 12:05:25 +0200

    normed logger and mapper

--- 1_wip/push_swap/src/logger/logger.c
+++ 1_wip/push_swap/src/logger/logger.c
@@ -6,55 +6,57 @@
 /*   By: abraekev <abraekev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 09:55:54 by abraekev          #+#    #+#             */
-/*   Updated: 2024/10/15 09:49:45 by abraekev         ###   ########.fr       */
+/*   Updated: 2024/10/15 11:52:05 by abraekev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static char* get_op_str(t_op op)
+static char	*get_op_str(t_op op)
 {
-    char* strings[12];
+	char	*strings[12];
 
-    strings[0] = "zero op";
-    strings[1] = "pa";
-    strings[2] = "pb";
-    strings[3] = "sa";
-    strings[4] = "sb";
-    strings[5] = "ss";
-    strings[6] = "ra";
-    strings[7] = "rb";
-    strings[8] = "rr";
-    strings[9] = "rra";
-    strings[10] = "rrb";
-    strings[11] = "rrr";
-    if (op < 0 || op > RRR)
-        return "Invalid op";
-    return (strings[op]);
+	strings[0] = "zero op";
+	strings[1] = "pa";
+	strings[2] = "pb";
+	strings[3] = "sa";
+	strings[4] = "sb";
+	strings[5] = "ss";
+	strings[6] = "ra";
+	strings[7] = "rb";
+	strings[8] = "rr";
+	strings[9] = "rra";
+	strings[10] = "rrb";
+	strings[11] = "rrr";
+	if (op < 0 || op > RRR)
+		return ("Invalid op");
+	return (strings[op]);
 }
 
-static void print_lst_item(void* ptr) {
-    t_op op;
-
-    op = (t_op)(uintptr_t)ptr;
-    ft_printf(1, "%s\n", get_op_str(op));
-}
-
-void log_ops(t_ps* data, t_op op) {
-    t_list* new;
-
-    new = ft_lstnew((void*)(uintptr_t)op);
-    if (!new)
-        error(data);
-    ft_lstadd_back(&data->ops, new);
-}
-
-void print_log(t_ps* data)
+static void	print_lst_item(void *ptr)
 {
-    ft_lstiter(data->ops, print_lst_item);
+	t_op	op;
+
+	op = (t_op)(uintptr_t)ptr;
+	ft_printf(1, "%s\n", get_op_str(op));
 }
 
-t_op op_from(t_list* node)
+void	log_ops(t_ps *data, t_op op)
 {
-    return ((t_op)(uintptr_t)node->content);
+	t_list	*new;
+
+	new = ft_lstnew((void *)(uintptr_t)op);
+	if (!new)
+		error(data);
+	ft_lstadd_back(&data->ops, new);
+}
+
+void	print_log(t_ps *data)
+{
+	ft_lstiter(data->ops, print_lst_item);
+}
+
+t_op	op_from(t_list *node)
+{
+	return ((t_op)(uintptr_t)node->content);
 }
--- 1_wip/push_swap/src/push_swap.c
+++ 1_wip/push_swap/src/push_swap.c
@@ -6,27 +6,19 @@
 /*   By: abraekev <abraekev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 13:52:07 by abraekev          #+#    #+#             */
-/*   Updated: 2024/10/14 11:04:36 by abraekev         ###   ########.fr       */
+/*   Updated: 2024/10/15 11:42:32 by abraekev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int argc, char** argv)
+int	main(int argc, char **argv)
 {
-  t_ps data;
+	t_ps	data;
 
-  // 1. initiate data & sort
-  init_data(validate_arguments(argc, argv), argc, argv, &data);
-
-  // 2. sort the data
-  sort(&data);
-
-  // 3. print operation log
-  print_log(&data);
-
-  // 4. cleanup
-  free_data(&data);
-
-  exit(EXIT_SUCCESS);
+	init_data(validate_arguments(argc, argv), argc, argv, &data);
+	sort(&data);
+	print_log(&data);
+	free_data(&data);
+	exit(EXIT_SUCCESS);
 }
