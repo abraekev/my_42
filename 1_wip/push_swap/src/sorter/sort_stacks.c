42ea585b1c61ca1b011572f4fb33ccc5fb4e0e6a
abraekev <arnout.braekevelt@gmail.com>
2024-10-21 16:29:31 +0200

    pseudo update

--- 1_wip/push_swap/src/sorter/sort_stacks.c
+++ 1_wip/push_swap/src/sorter/sort_stacks.c
@@ -6,21 +6,53 @@
 /*   By: abraekev <abraekev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 14:17:46 by abraekev          #+#    #+#             */
-/*   Updated: 2024/10/21 15:56:11 by abraekev         ###   ########.fr       */
+/*   Updated: 2024/10/21 16:29:19 by abraekev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int get_target(t_ps *data, int node)
+{
+    // closest smaller number to node in b, else its the max value in b.
+    int target;
+    int rank;
+    int delta;
+
+    target = -1;
+    rank = 1;
+    delta = INT_MAX;
+    while(rank <= data->b.size)
+    {
+        if (value(&data->b, rank) < node && 
+            node - value(&data->b, rank) < delta)
+        {
+            target = rank;
+            delta = node - value(&data->b, rank);
+        }
+        rank++;
+    }
+    if (target == -1)
+        // create get max value or rank of stack b.
+        return get_max();
+    return target;
+}
+
 void    lowest_cost_push_desc_b(t_ps *data)
 {
-    while(1) // foreach node in a
-    {
-    // 1. get_target()
-    // 2. calc_cost()
-    // 3. update stack->cheapest if cheaper
+    int rank;
 
-    // if cost is 0, use this immediately.
+    rank = 1;
+    while(rank <= data->a.size) // foreach node in a
+    {
+        // 1. get_target()
+        get_target(data, value(&data->a, rank));
+        
+        // 2. calc_cost()
+        // 3. update stack->cheapest if cheaper
+
+        // if cost is 0, use this immediately.
+        rank++;
     }
 
     // 4. execute the push:
